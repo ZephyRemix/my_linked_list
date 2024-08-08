3d2e0ab9185bd8831a69b5c75d0de93454835630
ZephyRemix <hunyaochong@gmail.com>
2024-08-08 16:01:21 +0800

    Add methods to get size of linkedlist, head and tail nodes

--- linked_list.rb
+++ linked_list.rb
@@ -1,10 +1,12 @@
 require_relative 'node'
+require 'pry-byebug'
 
 class LinkedList 
-  attr_accessor :head
+  attr_accessor :head, :tail
 
   def initialize
     self.head = nil
+    self.tail = nil
   end
 
   def append(value)
@@ -14,10 +16,11 @@ class LinkedList
     # if head is created, loop to the last node and append to the last node
     tmp = self.head
     while (tmp.next_node != nil)
-      tmp.next_node
+      tmp = tmp.next_node
     end
-    # assign last_node.next = Node.new(value)
-    tmp.next_node = Node.new(value)
+    # add new node
+    self.tail = Node.new(value)
+    tmp.next_node = self.tail
   end
 
   def prepend(value)
@@ -25,15 +28,36 @@ class LinkedList
     self.head = new_head
   end
 
+  def size
+    count = 0
+    self.for_each { count += 1 }
+    return count
+  end
+
+  private
+
+  def for_each
+    tmp = self.head
+
+    while (tmp != nil)
+      yield(tmp)
+      tmp = tmp.next_node
+    end
+  end
+
 end
 
 list = LinkedList.new
+
 # p list
 list.append('dog')
-
 list.prepend('cat')
-p list.inspect
-# list.append('parrot')
+# p list.inspect
+
+list.append('parrot')
+puts list.size
+puts list.head.value
+puts list.tail.value
 # list.append('hamster')
 # list.append('snake')
 # list.append('turtle')
